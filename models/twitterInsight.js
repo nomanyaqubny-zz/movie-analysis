4cce178bb1d5961ed9f4d8d14221bfd2fe25dd07
Noman Yaqub <nyaqub@de.ibm.com>
2015-09-28 12:54:07 +0000

    check with: db insert now

--- models/twitterInsight.js
+++ models/twitterInsight.js
@@ -69,18 +69,18 @@ TwitterInsight.prototype = {
                     var times =  Math.ceil(count / MAX_TWEETS);
                     async.timesSeries(times, function(n, next) {
                         retrieveInsight(url, query, function(err, data) {
-                            //if (!err && data['tweets'].length > 0) {
-                                //insertTweets(db, tableName, data['tweets'], function(err, message, rows) {
-                                    //if(!err) {
-                                        progress += 500;
+                            if (!err && data['tweets'].length > 0) {
+                                insertTweets(db, tableName, data['tweets'], function(err, message, rows) {
+                                    if(!err) {
+                                        progress += rows;
                                         process.stdout.write("\rso far: " + progress);
-                                    //}
-                                    next(false, null);
-                                //});
-                            //} else {
-                              //console.log(err);
+                                    }
+                                    next(err, data);
+                                });
+                            } else {
+                              console.log(err);
                               //callback(err, progress)
-                           //}
+                           }
                         }, (n*MAX_TWEETS));
                     }, function(err, data) {
                         return callback(err, progress);
