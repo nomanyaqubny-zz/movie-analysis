f2c2a1e21657155d0fc9d4cacd44659e26e5f616
Torsten Steinbach <torsten@de.ibm.com>
2015-10-15 14:58:05 +0000

    Added BSD license to entire code base

--- public/javascripts/bootstrap-select.js
+++ public/javascripts/bootstrap-select.js
@@ -1,3 +1,14 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015, IBM Corp.
+// All rights reserved.
+//
+// Distributed under the terms of the BSD Simplified License.
+//
+// The full license is in the LICENSE file, distributed with this software.
+//
+// You may not use this file except in compliance with the License.
+//------------------------------------------------------------------------------
+
 (function ($) {
   'use strict';
 
